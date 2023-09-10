e425ad91cbafbaf1ffc8606f2706369c3b71206b
watanabeyou0409 <w.you0525@ezweb.ne.jp>
2023-09-10 10:25:40 +0900

    カレンダー削除機能の追加

--- routes/web.php
+++ routes/web.php
@@ -36,4 +36,5 @@ Route::middleware([
         return view('full-calendar');
     })->name('calendar');
     Route::get('/calendar/action', [FullCalendarController::class, 'index']);
+    Route::post('calendar/action', [FullCalendarController::class, 'action']);
 });
